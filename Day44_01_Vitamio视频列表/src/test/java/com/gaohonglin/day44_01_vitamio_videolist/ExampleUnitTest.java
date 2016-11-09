397fa099a00b8e4eff7bb517a40761e98fdcf0f5
YunZFX <1109030540@qq.com>
2016-11-09 22:24:18 +0800

    Initial commit

---
+++ Day44_01_Vitamio视频列表/src/test/java/com/gaohonglin/day44_01_vitamio_videolist/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.gaohonglin.day44_01_vitamio_videolist;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
